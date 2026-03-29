8e201fcbf2b1b3b029c9820e86b015bf14e10766
DaniFGS <danifgs@example.com>
2026-03-28 23:40:09 -0300

    fix: adiciona script supabase em cadastro e relatorio, corrige css e refina pathing na vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express'); 
 const cors = require('cors');
 require('dotenv').config();
@@ -8,7 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 // Main Router Imports
-const mainRoutes = require('../src/routes/index');
+// Usando caminhos absolutos para garantir que a Vercel encontre os arquivos movidos para /src
+const mainRoutes = require(path.join(__dirname, '../src/routes/index'));
 
 app.use('/api', mainRoutes);
 
